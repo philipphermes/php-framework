dad64cc1b480714b220b593ee38eb4f134a7a4a0
Philipp Anton Hermes <philipphermes2001@gmail.com>
2023-05-19 07:37:22 +0200

    changed styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.{html,html.twig,js}"],
+  content: ["./templates/**/*.{html,html.twig}"],
   theme: {
     extend: {},
   },
